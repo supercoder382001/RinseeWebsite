484da48354e782992b1245e77871a776cffcdd2b
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-09 17:42:24 +0530

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -1,33 +1,38 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
-    const queryParams = new URLSearchParams(window.location.search);
+    const queryParams = useSearchParams();
     const mid = queryParams.get('mid');
     const muid = queryParams.get('muid');
     const mno = queryParams.get('mno');
     const amount = queryParams.get('amount'); 
     const handleClick = async () => {
     try {
-        const body=req.data;
+        
         const response = await axios.post("/api/create", {
         merchantTransactionId: mid,
         merchantUserId: muid,
         mobileNumber: mno,
         amount: amount,
       });
-      window.location.href = response.data.data.instrumentResponse.redirectInfo.url;
-    //   window.open(
-    //     response.data.data.instrumentResponse.redirectInfo.url,
-    //     "_blank"
-    //   );
+      // if (!response.ok) {
+      //   window.location.href = response.data.data.instrumentResponse.redirectInfo.url;
+      // } else {
+      // console.error('Error:', response);
+      // }  
+      
+      window.open(
+        response.data.data.instrumentResponse.redirectInfo.url
+      );
     } catch (error) {
       console.error("Error making API request:", error);
     }
   };
   useEffect(() => {
     // Automatically call the redirect function when the page loads
-    handleRedirect();
+    handleClick();
   }, []); // Empty dependency array ensures it runs only once on page load
 
   return <div>Redirecting...</div>;
