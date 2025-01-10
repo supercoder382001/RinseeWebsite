c108e1c057de6ef78e970727933ce5e6b4a60811
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-11 03:02:23 +0530

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -6,12 +6,24 @@ export default function Page() {
     const queryParams = useSearchParams();
     const mid = queryParams.get('mid');
     let jsonObject={};
-    const jsonString=atob(mid);
-    jsonObject=JSON.parse(jsonString);
-    console.log(jsonObject);
+    // const jsonString=atob(mid);
+    // jsonObject=JSON.parse(jsonString);
+    // console.log(jsonObject);
     // const muid = queryParams.get('muid');
     // const mno = queryParams.get('mno');
-    // const amount = queryParams.get('amount'); 
+    // const amount = queryParams.get('amount');
+    if (mid) {
+      try {
+        // Decode the Base64-encoded token
+        const jsonString = atob(mid);
+        // Parse the decoded string into a JSON object
+        jsonObject = JSON.parse(jsonString);
+        console.log(jsonObject);
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        alert('Invalid Base64-encoded token or JSON format.');
+      }
+    } 
     const handleClick = async () => {
     try {
       //   const response = await axios.post("/api/create", {
@@ -20,10 +32,10 @@ export default function Page() {
       //   mobileNumber: mno,
       //   amount: amount,
       // });      
-      // window.open(
-      //   response.data.data.instrumentResponse.redirectInfo.url,
-      //   "_parent"
-      // );
+      window.open(
+        jsonObject.data.data.instrumentResponse.redirectInfo.url,
+        "_parent"
+      );
     } catch (error) {
       console.error("Error making API request:", error);
     }
