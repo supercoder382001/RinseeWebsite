f7f95c497b801dbce39c4f69e3a1555db6b8745c
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-21 08:23:37 +0530

    Update create.js

--- pages/api/create.js
+++ pages/api/create.js
@@ -10,7 +10,7 @@ export default async function POST(req,res) {
       merchantTransactionId: data.merchantTransactionId,
       merchantUserId: data.merchantUserId,
       amount: data.amount,
-      redirectUrl: `https://rinsee.in`,
+      redirectUrl: `https://rinsee.in/api/hello`,
       redirectMode: "POST",
       callbackUrl: "https://rinsee.in/api/result",
       mobileNumber: data.mobileNumber,
